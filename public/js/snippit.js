ceafec5ac29e253f85651d075fe6b38abdbb1359
lex Phumirat <lexphumirat@gmail.com>
2017-10-16 00:31:51 -0700

    newupdate5

---
+++ public/js/snippit.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function() {
+    $('#example').DataTable();
+    $('select').addClass('mdb-select');
+    $('.mdb-select').material_select();
+});
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
-
+// const $ = require('jQuery');
 const { Schema } = mongoose;
 const port = process.env.PORT || 8000;
 const app = express();
 
 
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -48,13 +49,14 @@ const appSchema = new Schema ({
     },
     appserver: String,
     appvendor: String,
-    appstatus: String,
     appzone: String,
     applevel: String,
     appdesc: String,
     appdataowner: String,
     appverison: String,
     appbackup: String,
+    appmaint: String,
+    appvensupport: String,
 
 
 
@@ -100,6 +102,20 @@ app.get('/analysts', function ( request, response){
     });
 });
 
+//admin dashboard
+app.get('/admin/analysts', function ( request, response){
+    Analyst.find({})
+     .populate('analysts')
+     .exec(function(err, analysts){
+        if (err){
+            console.log(err);
+            throw err;
+        }
+
+        response.render('admin/analystsindex.ejs', { analysts });
+    });
+});
+
  app.get('/analysts/new', function(request, response){
      response.render('analysts/new');
  });
@@ -137,6 +153,29 @@ app.get('/apps', function (request, response){
     });
 });
 
+//admin dashboard
+app.get('/admin', function (request, response){
+    Application.find({})
+    .populate('_analysts')
+    .then(applications => {
+        response.render('admin/appindex', { applications });
+    })
+    .catch(function(error){
+        console.log("error in adding apps");
+    });
+});
+
+//admin dashboard analyst
+app.get('/admin/analyst', function (request, response){
+    Analyst.find({})
+    .populate('_analysts')
+    .then(analysts => {
+        response.render('admin/analystsindex', { analysts });
+    })
+    .catch(function(error){
+        console.log("error in adding apps");
+    });
+});
 // app.get('/apps', function ( request, response){
 //     Application.find({}, function(err, applications){
 //         if (err){
@@ -158,6 +197,10 @@ app.post('/applications', function(request, response){
         applevel: request.body.applevel,
         appdesc: request.body.appdesc,
         appverison: request.body.appverison,
+        appmaint: request.body.appmaint,
+        appvensupport: request.body.appvensupport,
+        appbackup: request.body.appbackup,
+        appdataowner: request.body.appdataowner,
         _analysts: request.body._analysts
     })
 
@@ -204,7 +247,7 @@ app.get('/applications/new', function(request, response){
 
 });
 
-//show id route
+//show id route analyst
 app.get('/:id', (request, response) => {
     Analyst.findById(request.params.id)
     .populate('_applications')
@@ -215,13 +258,46 @@ app.get('/:id', (request, response) => {
     .catch(console.log);
 });
 
-app.get('/:id/app', (request, response) => {
+//admin id route analyst
+app.get('/admin/:id', (request, response) => {
+    Analyst.findById(request.params.id)
+    .populate('_applications')
+    .then(analyst => {
+        console.log(analyst);
+        response.render('admin/editanalyst', { analyst })
+    })
+    .catch(console.log);
+});
 
+app.get('/:id/app', (request, response) => {
     Application.findById(request.params.id)
-    .populate('analyst')
+    // Application.find({})
+    .populate('_analysts')
     .then(application => {
-        console.log(application);
-        response.render('apps/show', { application })
+        return Analyst.find({})
+        .then( analysts => {
+            console.log('application', application);
+            console.log('analysts', analysts);
+            response.render('apps/show', { application, analysts })
+        })
+
+    })
+    .catch(console.log);
+});
+
+//admin dashboard
+app.get('/:id/admin', (request, response) => {
+    Application.findById(request.params.id)
+    // Application.find({})
+    .populate('_analysts')
+    .then(application => {
+        return Analyst.find({})
+        .then( analysts => {
+            console.log('application', application);
+            console.log('analysts', analysts);
+            response.render('admin/editapp', { application, analysts })
+        })
+
     })
     .catch(console.log);
 });
@@ -235,6 +311,26 @@ app.post('/apps/:id/edit', (request, response) => {
     .catch(console.log)
 });
 
+
+//admin dashboard Edit
+app.post('/admin/:id/edit', (request, response) => {
+    Application.findByIdAndUpdate(request.params.id, request.body)
+    .then(() =>{
+        console.log('editsomething');
+        response.redirect('/admin');
+    })
+    .catch(console.log)
+});
+//admin dashboard Remove
+app.post('/apps/:id/remove', (request, response) => {
+    Application.findByIdAndRemove(request.params.id)
+    .then(() =>{
+        console.log('remove');
+        response.redirect('/admin');
+    })
+
+    .catch(console.log)
+})
 // app.get('apps/:id/remove', function(req, res){
 //     console.log('deleteing something');
 //   Application.remove({ _id: req.params.id }, function(err, application){
@@ -252,5 +348,14 @@ app.post('/apps/:id/remove', (request, response) => {
 
     .catch(console.log)
 })
+//admin dashboard Analyst edit
+app.post('/admin/analyst/:id/edit', (request, response) => {
+    Analyst.findByIdAndUpdate(request.params.id, request.body)
+    .then(() =>{
+        console.log('editsomething for analyst');
+        response.redirect('/admin/analyst');
+    })
+    .catch(console.log)
+});
 
 app.listen(port, () => console.log('listen on port 8000 ${ port }'));
